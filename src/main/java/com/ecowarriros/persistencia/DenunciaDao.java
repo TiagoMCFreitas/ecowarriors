9ac8cf2ce283a4253dfa3cc66c0a5fe56ef2807d
Tiago Marques de Oliveira <105756442+tiagoKurt@users.noreply.github.com>
2023-11-21 21:07:41 -0300

    feat: methods implementeds

--- src/main/java/com/ecowarriros/persistencia/DenunciaDao.java
+++ src/main/java/com/ecowarriros/persistencia/DenunciaDao.java
@@ -1,4 +1,25 @@
 package com.ecowarriros.persistencia;
+import java.util.ArrayList;
+
+import com.ecowarriros.modelos.Denuncia;
 import com.ecowarriros.persistencia.IDenunciaDao;
-public class DenunciaDao {
+public class DenunciaDao implements IDenunciaDao{
+
+    @Override
+    public void cadastroDenuncia(Denuncia denuncia) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cadastroDenuncia'");
+    }
+
+    @Override
+    public void consultarDenuncia(Denuncia denuncia) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'consultarDenuncia'");
+    }
+
+    @Override
+    public ArrayList<Denuncia> listagemDenuncia() throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listagemDenuncia'");
+    }
 }
